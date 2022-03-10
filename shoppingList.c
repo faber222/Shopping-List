e7c5ce15c4e7580515bc7f68dc056a8dbd77a5a7
faber222 <faberbernardo78@gmail.com>
2022-03-10 18:55:38 -0300

    Final Commit

--- shoppingList.c
+++ shoppingList.c
@@ -1,121 +1,135 @@
-//#include <conio.h>
+/*
+AUThOR: Faber Bernardo júnior & Igor Silva Vieira
+DATA: 01/03/2022
+PROGRAM SYNOPSIS: make a shopping list, just using the terminal
+INPUT DATA: the struct listFile
+OUTPUT DATA: the struct fileWrite
+*/
+
 #include <locale.h>
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int imput;
 struct fileWrite {
-  char copyFile[1000];
-  char fileName[1000];
+  char copyFile[100];
+  char fileName[100];
 } fp;
 
 struct listFile {
   int number;
-  char character[1000];
+  char character[100];
+  int amount;
 } list;
 
-FILE *file;
-FILE *copiedFile;
+FILE* mainFile;
 
-void flush_in() {  // clean the buffer
-  int ch;
-  while ((ch = fgetc(stdin)) != EOF && ch != '\n') {
+FILE* fileOpen(char* name, char* mode) {  // function for open the mainFile
+  FILE* test = fopen(name, mode);         // fopen manipulate the FILE pointer
+  if (test == NULL) {                     // test if it doesn't open, will alarm
+    printf("\n\nError: The %s mainFile was not opened\n", fp.fileName);
+    system("pause");  // it only works on windows
+    exit(0);
+  }
+  return test;
+};
+
+void create() {
+  list.number = 0;
+  char numberList[100];
+  char compare[100];
+  while (list.number == 0) {
+    printf("How many items do you want to add?\n");
+    scanf("%s", numberList);
+    list.number =
+        atoi(numberList);  // This function returns the converted integral
+                           // number as an int value. If no valid conversion
+                           // could be performed, it returns zero.
+    if (list.amount == 0) {
+      printf("Please enter a number!\n");
+    }
+  }
+
+  for (int i = 1; i < list.number + 1; i++) {
+    printf("Item - %d\n", i);
+    scanf("%s", list.character);  // scan the item for add in the mainFile
+    list.amount = 0;
+    while (list.amount == 0) {
+      printf("Amount - %s\n", list.character);
+      scanf("%s", compare);  // scan the item for add in the mainFile
+      list.amount =
+          atoi(compare);  // This function returns the converted integral number
+                          // as an int value. If no valid conversion could be
+                          // performed, it returns zero.
+      if (list.amount == 0) {
+        printf("Please enter a number!\n");
+      }
+    }
+
+    fprintf(mainFile, "- %s / amount: %d\n", list.character,
+            list.amount);  // print into the mainFile can be replaced by fputs
   }
 };
 
 void write() {
   printf("File to be created\n");
   scanf("%s", fp.fileName);
-  file = fopen(fp.fileName, "w");  // if not exist, will create
+  mainFile = fileOpen(fp.fileName, "w");  // if not exist, will create for write
 
-  if (file != NULL)  // if anything is wrong, will alarm
-    printf("\n%s file was opened successfully\n", fp.fileName);
-  else {
-    printf("\n\nError: The %s file was not opened\n", fp.fileName);
-    system("pause");
-    exit(0);
-  }
-  printf("How many items do you want to add?\n");
-  scanf("%d", &list.number);
-
-  for (int i = 1; i < list.number + 1; i++) {
-    printf("Item - %d\n", i);
-    scanf("%s", list.character);  // scan the item for add in the file
-    fprintf(file, "%d - %s\n", i, list.character);  // print into the file
-  }
-  fclose(file);  // close the file
+  create();
+  fclose(mainFile);  // close the mainFile
 };
 
 void read() {
   printf("File to be read\n");
   scanf("%s", fp.fileName);
-  file = fopen(fp.fileName, "r");  // will open the file *.txt
+  mainFile =
+      fileOpen(fp.fileName, "r");  // will open the mainFile *.txt for read
 
-  if (file != NULL)  // if anything is wrong, will alarm
-    printf("\n%s file was opened successfully\n", fp.fileName);
-  else {
-    printf("\n\nError: The %s file was not opened\n", fp.fileName);
-    system("pause");
-    exit(0);
-  }
   char word[100];  // char to storage the word
-  while (fgets(word, 100, file) !=
-         NULL)  // fgets will capture the word to repeat while !n NULL
+  while (fgets(word, 100, mainFile) !=
+         NULL)  // fgets will get the entire string of file while != NULL
   {
     printf("%s", word);  // print the read word while looping
   }
-  fclose(file);  // close the file
+
+  fclose(mainFile);  // close the mainFile
 };
 
 void alter() {
   printf("File to be alter\n");
   scanf("%s", fp.fileName);
-  file = fopen(fp.fileName, "a");  // will open the file *.txt
+  mainFile =
+      fileOpen(fp.fileName, "a");  // will open the mainFile *.txt for alter
 
-  if (file != NULL)  // if anything is wrong, will alarm
-    printf("\n%s file was opened successfully\n", fp.fileName);
-  else {
-    printf("\n\nError: The %s file was not opened\n", fp.fileName);
-    system("pause");
-    exit(0);
-  }
-  printf("How many items do you want to add?\n");
-  scanf("%d", &list.number);
-  for (int i = 1; i < list.number + 1; i++) {
-    printf("Item - %d\n", i);
-    scanf("%s", list.character);  // scan the item for add in the file
-    fprintf(file, "%d - %s\n", i, list.character);  // print into the file
-  }
-  fclose(file);  // close the file
+  create();
+  fclose(mainFile);  // close the mainFile
 };
 
 void copy() {
+  FILE* copiedFile;
   printf("File to be copy\n");
   scanf("%s", fp.fileName);
-  file = fopen(fp.fileName, "r");
-
-  if (file != NULL)  // if anything is wrong, will alarm
-    printf("\n%s file was opened successfully\n", fp.fileName);
-  else {
-    printf("\n\nError: The %s file was not opened\n", fp.fileName);
-    system("pause");
-    exit(0);
-  }
+  mainFile =
+      fileOpen(fp.fileName, "r");  // will open the mainFile *.txt for read
 
   printf("File name to be copied\n");
   scanf("%s", fp.copyFile);
-  copiedFile = fopen(fp.copyFile, "w");
-  while (fgets(list.character, 1000, file) != NULL) {
-    fputs(list.character, copiedFile);
+  copiedFile =
+      fopen(fp.copyFile, "w");  // will open the copiedFile *.txt for  write
+
+  while (fgets(list.character, 100, mainFile) !=
+         NULL) {  // fgets will get the entire string of file while != NULL
+    fputs(list.character, copiedFile);  // fputs will put the string of
+                                        // list.character in the copiedFile
   }
-  fclose(file);
-  fclose(copiedFile);
+
+  fclose(mainFile);    // close the mainFile
+  fclose(copiedFile);  // close the copiedFile
 };
 
 int main(void) {
   setlocale(LC_ALL, "pt_BR.UTF-8");  // set language to pt-BR
+  int imput;
   do {
     printf("\nSelect one of the options below");
     printf("\n(1) to create a list");
@@ -125,24 +139,20 @@ int main(void) {
     printf("\n(5) to exit the program\n");
     scanf("%d", &imput);  // scan the type valye
     switch (imput) {
-      case 1:                              //
-        setlocale(LC_ALL, "pt_BR.UTF-8");  // set language to pt-BR
-        write();                           // execute the function write
+      case 1:
+        write();  // The function write, create the shopping list file
         break;
       case 2:
-        setlocale(LC_ALL, "pt_BR.UTF-8");  // set language to pt-BR
-        read();                            // execute the function read
+        read();  // The function read, show the file datas in the terminal
         break;
       case 3:
-        setlocale(LC_ALL, "pt_BR.UTF-8");  // set language to pt-BR
-        alter();                           // execute the function alter
+        alter();  // The function alter, just imput more datas into the file
         break;
       case 4:
-        setlocale(LC_ALL, "pt_BR.UTF-8");
-        copy();
+        copy();  // The function copy, create a copy of the file and save with a
+                 // another name
         break;
       case 5:
-        setlocale(LC_ALL, "pt_BR.UTF-8");  // set language to pt-BR
         printf("\n\nFinish program!\n\n");
         return 0;
         break;
@@ -157,6 +167,6 @@ int main(void) {
   } while (imput <= 4);
 
   printf("\n");
-  system("pause");
+  system("pause");  // it only works on windows
   return 0;
 }
